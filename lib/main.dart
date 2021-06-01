f8a9e67999c4ddfecc5785f48c0cd61eb260448d
Awais Haleem <awaishaleem20@gmail.com>
2021-06-01 15:09:16 +0500

    Class#2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -8,90 +8,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SingleChildScrollView(
-                  child: Container(
-              child: Center(
-            child: Column(
-              children: [
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  ),
-                
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.purple,
-                  )
-                )
-              ],
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Center(child: Text('Login')),
             ),
-          )),
-        ),
-      ),
-    );
+            body: Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Center(
+                    child: Container(
+                      height: 40,
+                      child: TextField(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: Container(
+                      height: 30,
+                      child: TextField(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Login'))
+                ],
+              ),
+            )));
   }
 }
