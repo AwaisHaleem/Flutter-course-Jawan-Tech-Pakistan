853eb9e823a2646aa27f520d22b484eb23a1e2d1
Awais Haleem <awaishaleem20@gmail.com>
2021-06-02 13:16:16 +0500

    Align widget and Container's propertise

--- lib/home.dart
+++ lib/home.dart
@@ -9,13 +9,22 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.red,
+      body: Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          margin: EdgeInsets.only(top: 30),
+          padding: EdgeInsets.only(left: 20, top: 20),
+          child: Text("Box Decoration"),
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+              // color: Colors.red,
+              // gradient: LinearGradient(colors: [Colors.red, Colors.yellow]),
+              // color: Color(0xff756cc4),
+              color: Colors.yellow[400],
+              borderRadius: BorderRadius.circular(20)),
+        ),
       ),
-   
-      
     );
   }
 }
