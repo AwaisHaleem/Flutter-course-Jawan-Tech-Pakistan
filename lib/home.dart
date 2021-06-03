6fc63a0d248982260ef220a32ffeb730c7ef02c4
Awais Haleem <awaishaleem20@gmail.com>
2021-06-03 22:55:03 +0500

    CircleAvatar widget and Asset and Network images

--- lib/home.dart
+++ lib/home.dart
@@ -10,10 +10,11 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Text(
-      "Hello World!",
-      style: TextStyle(
-          fontSize: 30, color: Colors.red, fontWeight: FontWeight.bold),
-    )));
+      child: CircleAvatar(
+        radius: 100,
+        // backgroundImage: NetworkImage('https://images.unsplash.com/photo-1553356084-58ef4a67b2a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'),
+        backgroundImage: AssetImage('pg.jpeg'),
+      ),
+    ));
   }
 }
