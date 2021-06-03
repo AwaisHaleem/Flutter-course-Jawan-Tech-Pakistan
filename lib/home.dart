03b908fb69d47eb054faaa36d028182a61ac90fc
Awais Haleem <awaishaleem20@gmail.com>
2021-06-03 22:37:18 +0500

    Text Styling

--- lib/home.dart
+++ lib/home.dart
@@ -9,22 +9,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.centerLeft,
-        child: Container(
-          margin: EdgeInsets.only(top: 30),
-          padding: EdgeInsets.only(left: 20, top: 20),
-          child: Text("Box Decoration"),
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-              // color: Colors.red,
-              // gradient: LinearGradient(colors: [Colors.red, Colors.yellow]),
-              // color: Color(0xff756cc4),
-              color: Colors.yellow[400],
-              borderRadius: BorderRadius.circular(20)),
-        ),
-      ),
-    );
+        body: Center(
+            child: Text(
+      "Hello World!",
+      style: TextStyle(
+          fontSize: 30, color: Colors.red, fontWeight: FontWeight.bold),
+    )));
   }
 }
