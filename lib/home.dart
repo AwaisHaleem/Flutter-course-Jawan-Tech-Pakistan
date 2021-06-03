1f2d6b2184fb413cf2062fbcea601460b4c70da9
Awais Haleem <awaishaleem20@gmail.com>
2021-06-03 23:48:14 +0500

    Stack and positioned widget

--- lib/home.dart
+++ lib/home.dart
@@ -9,12 +9,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: CircleAvatar(
-        radius: 100,
-        // backgroundImage: NetworkImage('https://images.unsplash.com/photo-1553356084-58ef4a67b2a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'),
-        backgroundImage: AssetImage('pg.jpeg'),
-      ),
+        body: Stack(
+      children: [
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.yellow,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.purple,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+        ),
+        Positioned(
+          left: 20,
+          top: 20,
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.black,
+          ),
+        ),
+      ],
     ));
   }
 }
