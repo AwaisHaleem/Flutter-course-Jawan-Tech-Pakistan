b880264dc636761dba6651863bc0956b2a7f00e7
Awais Haleem <awaishaleem20@gmail.com>
2021-06-04 22:24:22 +0500

    MediaQuery for responsivness

--- lib/home.dart
+++ lib/home.dart
@@ -9,50 +9,10 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.yellow,
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.purple,
-          ),
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
-        ),
-        Positioned(
-          left: 20,
-          top: 20,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.black,
-          ),
-        ),
-      ],
+        body: Container(
+      color: Colors.purple,
+      height: MediaQuery.of(context).size.height * 0.5,
+      width: MediaQuery.of(context).size.width * 0.25,
     ));
   }
 }
