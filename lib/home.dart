95d7404daf41fad49a4250e27a9b9b5f3eca9db3
Awais Haleem <awaishaleem20@gmail.com>
2021-06-11 23:52:23 +0500

    TabBar and bottom

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jawan_tech_flutter_course/login.dart';
+import 'package:jawan_tech_flutter_course/register.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,26 +8,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List names = <String>['Ali', 'Ahmad', 'Ghazali', 'mosa'];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Drawer Example'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            Container(
-                color: Colors.yellow, child: DrawerHeader(child: Text('abs'))),
-            ListTile(
-              title: Text('budjet'),
-            ),
-            ListTile(
-              title: Text('budjet'),
-            )
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            title: Text('Hello'),
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black,
+              labelColor: Colors.white,
+              indicatorColor: Colors.red,
+              tabs: [Text('Login'), Text('Register')],
+            )),
+        body: TabBarView(
+          children: [Login(), Register()],
         ),
       ),
     );
