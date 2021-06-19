403dafc9e2338cfb8b7186c912fc148be725c3b9
Awais Haleem <awaishaleem20@gmail.com>
2021-06-19 10:58:57 +0500

    shrinkWrap and physics continues

--- lib/home.dart
+++ lib/home.dart
@@ -6,43 +6,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<dynamic> lst = [1, 2, 3, 4, 5, 6, 7, 8];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-      child: Column(children: [
-        ListView(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              color: Colors.amber,
-              height: 200,
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              color: Colors.amber,
-              height: 200,
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              color: Colors.amber,
-              height: 200,
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              color: Colors.amber,
-              height: 200,
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              color: Colors.amber,
-              height: 200,
-            ),
-          ],
-        )
-      ]),
+      child: Center(
+        child: Column(children: [
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Colors.red,
+            height: 200,
+            width: 200,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Colors.red,
+            height: 200,
+            width: 200,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Colors.red,
+            height: 200,
+            width: 200,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            color: Colors.red,
+            height: 200,
+            width: 200,
+          ),
+          GridView.count(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            children: List.generate(lst.length, (index) {
+              return Container(
+                  color: Colors.amber,
+                  child: Center(child: Text('${lst[index]}')));
+            }),
+          )
+        ]),
+      ),
     ));
   }
 }
