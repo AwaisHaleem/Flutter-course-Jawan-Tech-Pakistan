f2c426039784a3b35cc3188af0ea2f082f5c1279
Awais Haleem <awaishaleem20@gmail.com>
2021-06-19 10:17:53 +0500

    shrinkWrap and physics properties of scrollable widgets

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jawan_tech_flutter_course/login.dart';
-import 'package:jawan_tech_flutter_course/register.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,37 +6,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var text = '';
-  one() {
-    setState(() {
-      text = '1';
-    });
-  }
-
-  two() {
-    setState(() {
-      text = '2';
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Text $text'),
-              SizedBox(height: 20),
-              ElevatedButton(onPressed: one, child: Text('make 1')),
-              SizedBox(height: 20),
-              ElevatedButton(onPressed: two, child: Text('make 2')),
-            ],
-          ),
-        ),
-      ),
-    );
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(children: [
+        ListView(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              color: Colors.amber,
+              height: 200,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              color: Colors.amber,
+              height: 200,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              color: Colors.amber,
+              height: 200,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              color: Colors.amber,
+              height: 200,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              color: Colors.amber,
+              height: 200,
+            ),
+          ],
+        )
+      ]),
+    ));
   }
 }
